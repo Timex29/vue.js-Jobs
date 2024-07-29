4784ab1b2b1b3ff629f9f8ae95b7cc675e6e3911
@Cornelio <jun23.llagas@gmail.com>
2024-07-29 20:02:46 +0800

    First release of vue Jobs tutorial from Traversy Media

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Navbar from "@/components/Navbar.vue";
+
+import { RouterView } from "vue-router";
+
+
+/**
+   * import Hero from "@/components/Hero.vue";
+  import HomeCards from "@/components/HomeCards.vue";
+  import JobsListings from "@/components/JobsListings.vue";
+ */
+
+</script>
+
+<template>
+  <Navbar />
+  <RouterView />
+  <!--
+  <Hero />
+  <HomeCards />
+  <JobsListings :limit="3" :showButton="true" />
+  -->
+</template>
